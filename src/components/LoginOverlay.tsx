65591b0546737c78aa1e873fc097fc0e4b6ff65a
aayushker <singhaayushker@gmail.com>
2024-05-02 20:15:31 +0530

    Added Login Option

---
+++ src/components/LoginOverlay.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  Button,
+  Input,
+} from "@nextui-org/react";
+import Login from "./Login";
+
+export default function LoginOverlay() {
+
+  // const content = (
+  //   <PopoverContent className="w-[240px]">
+  //     {(titleProps) => (
+  //       <Login />
+  //     )}
+  //   </PopoverContent>
+  // );
+
+  return (
+    <div className="flex flex-wrap gap-4">
+      <Popover
+        key="login"
+        showArrow
+        offset={10}
+        placement="bottom"
+        backdrop="blur"
+      >
+        <PopoverTrigger>
+          <Button color="warning" variant="flat" className="capitalize">
+            Login
+          </Button>
+        </PopoverTrigger>
+        {/* {content} */}
+        <Login />
+      </Popover>
+    </div>
+  );
+}
